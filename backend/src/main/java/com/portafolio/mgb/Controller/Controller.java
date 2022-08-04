77025d2cc3f2699c82840d4ca6b7bcbf0022936e
Leonel-H29 <leonelherreradkno@gmail.com>
2022-08-04 19:52:49 -0300

    Realizando consultas POST y GET

--- backend/src/main/java/com/portafolio/mgb/Controller/Controller.java
+++ backend/src/main/java/com/portafolio/mgb/Controller/Controller.java
@@ -1,12 +1,20 @@
 package com.portafolio.mgb.Controller;
 
+import com.portafolio.mgb.model.Persona;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+    
+    List<Persona> listPersonas = new ArrayList();
     
     @GetMapping ("/hello/{nombre}")
     public String Hello(@PathVariable String nombre){
@@ -15,5 +23,15 @@ public class Controller {
     @GetMapping("/chau")
     public String GoodBye(@RequestParam String nombre){
         return "Good Bye " + nombre;
+    }
+    
+    @PostMapping("/new/persona")
+    public void agregarPersona(@RequestBody Persona pers){
+        listPersonas.add(pers);
+    }
+    @GetMapping("/ver/personas")
+    @ResponseBody   
+    public List<Persona> LookPersonas(){
+        return listPersonas;
     }
 }
--- backend/src/main/java/com/portafolio/mgb/model/Persona.java
+++ backend/src/main/java/com/portafolio/mgb/model/Persona.java
@@ -1,13 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.portafolio.mgb.model;
 
-/**
- *
- * @author leonel
- */
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter @Setter
+@Entity
 public class Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+     
+    private String nombre;
+    private String apellido;
+    
+    public Persona(){}
+    
+    public Persona(Long id, String nombre,String apellido){
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+    
+    
+    
+    
+    
     
 }
