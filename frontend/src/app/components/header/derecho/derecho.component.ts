ba5ca2caa0a4cfb6ff601f71e6a717ef61c3ce8c
Leonel-H29 <leonelherreradkno@gmail.com>
2022-08-06 15:29:10 -0300

    Agrego función para ir al login

--- frontend/src/app/components/header/derecho/derecho.component.ts
+++ frontend/src/app/components/header/derecho/derecho.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-derecho',
   templateUrl: './derecho.component.html',
-  styleUrls: ['./derecho.component.css']
+  styleUrls: ['./derecho.component.css'],
 })
 export class DerechoComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  login() {
+    this.router.navigate(['/login']);
   }
-
+  ngOnInit(): void {}
 }
