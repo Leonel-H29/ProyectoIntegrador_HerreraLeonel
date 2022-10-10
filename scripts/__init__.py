5a1219841f57edfc587d66a31ec26d687c46c968
Leonel-H29 <48606307+Leonel-H29@users.noreply.github.com>
2022-10-10 11:30:21 -0300

    Carga automatica de Tipos de empleos

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,9 +1,11 @@
-from random_personas import random_personas
+#from random_personas import random_personas
+from random_tipo_empleo import random_tipo_empleo
 
 
 if __name__ == "__main__":
 
     api_url = "http://localhost:8080/"
 
-    random_personas(url=api_url)
+    #random_personas(url=api_url)
+    random_tipo_empleo(url=api_url)
     
---
+++ scripts/random_tipo_empleo.py
@@ -0,0 +1,29 @@
+import requests
+#from faker import Faker
+
+
+tipo_empleo = sorted([
+    'FULL TIME PRESENCIAL',
+    'FULL TIME HIBRIDO',
+    'FULL TIME REMOTO' ,
+    'PART TIME PRESENCIAL',
+    'PART TIME HIBRIDO',
+    'PART TIME REMOTO',
+    'FREELANCER'
+])
+
+
+def random_tipo_empleo(cantidad: int = len(tipo_empleo), url: str = "http://127.0.0.1:8080/")->None :
+    url += "tipoemp/create"
+    #fake  = Faker()
+    print("Agregando {} tipo_empleoas".format(cantidad))
+    for i in range(0, cantidad):
+        tipo = {}
+        tipo["tipo"] = tipo_empleo[i]
+        r = requests.post(url, json=tipo)
+        # print(tipo_empleoa)
+        print(r.status_code, "tipo de empleo agregado {}".format(i))
+
+if __name__ == "__main__":
+    random_tipo_empleo()
+
