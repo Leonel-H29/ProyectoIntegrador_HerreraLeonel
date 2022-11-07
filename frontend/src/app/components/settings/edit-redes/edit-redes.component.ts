e3cc7767189f750a8f3b44ddc6eb682d3370b9b5
Leonel-H29 <48606307+Leonel-H29@users.noreply.github.com>
2022-11-07 11:08:54 -0300

    Update

--- frontend/src/app/components/settings/edit-redes/edit-redes.component.ts
+++ frontend/src/app/components/settings/edit-redes/edit-redes.component.ts
@@ -10,7 +10,7 @@ import { persona } from 'src/app/model/persona.model';
 import { AuthService } from 'src/app/service/auth.service';
 import { PersonaService } from 'src/app/service/persona.service';
 import { TokenService } from 'src/app/service/token.service';
-
+/*
 interface Country {
   name: string;
   flag: string;
@@ -44,6 +44,7 @@ const COUNTRIES: Country[] = [
     population: 1409517397,
   },
 ];
+*/
 
 @Component({
   selector: 'app-edit-redes',
@@ -53,7 +54,7 @@ const COUNTRIES: Country[] = [
 export class EditRedesComponent implements OnInit, AfterViewInit {
   NombreRed: string = '';
   UrlWeb: string = '';
-  countries = COUNTRIES;
+  //countries = COUNTRIES;
 
   Opciones: string[] = [
     'Facebook',
