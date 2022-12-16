c92ec3224cc902232c39bc9d36221b0ea0c4984f
Leonel-H29 <leonelherreradkno@gmail.com>
2022-12-16 14:15:02 -0300

    Modifico el menu de redes sociales
    A medida de que el usuario va ingresando sus redes van a ir apareciendo en el menu

--- frontend/src/app/components/header/derecho/redes/redes.component.ts
+++ frontend/src/app/components/header/derecho/redes/redes.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Redes } from 'src/app/model/red';
+import { RedesService } from 'src/app/service/redes.service';
 
 @Component({
   selector: 'app-redes',
   templateUrl: './redes.component.html',
-  styleUrls: ['./redes.component.css']
+  styleUrls: ['./redes.component.css'],
 })
-export class RedesComponent implements OnInit {
+export class RedesComponent implements OnInit, AfterViewInit {
+  //showRedes = true;
+  idPersonaLogged: number = this.activatedRouter.snapshot.params['id'];
+  RedesUser: Redes[] = [];
+  constructor(
+    private activatedRouter: ActivatedRoute,
+    private changeDet: ChangeDetectorRef,
+    private redService: RedesService
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.changeDet.detectChanges();
   }
 
+  ngOnInit(): void {
+    this.getRedes();
+  }
+
+  getRedes(): void {
+    this.redService.ListaRedesByPersona(this.idPersonaLogged).subscribe(
+      (data) => {
+        this.RedesUser = data;
+      },
+      (err) => {
+        alert('Error al modificar el registro');
+        console.log('Error: ', err);
+        //this.router.navigate(['']);
+      }
+    );
+  }
 }
