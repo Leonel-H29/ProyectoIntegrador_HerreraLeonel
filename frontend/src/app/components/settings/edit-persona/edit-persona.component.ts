0a478438cd2afc939361226d32c3af60b262b113
Leonel-H29 <leonelherreradkno@gmail.com>
2022-12-26 22:01:00 -0300

    Update edit-persona.component.ts

--- frontend/src/app/components/settings/edit-persona/edit-persona.component.ts
+++ frontend/src/app/components/settings/edit-persona/edit-persona.component.ts
@@ -11,6 +11,7 @@ import { AuthService } from 'src/app/service/auth.service';
 import { ImageService } from 'src/app/service/image.service';
 import { PersonaService } from 'src/app/service/persona.service';
 import { TokenService } from 'src/app/service/token.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-edit-persona',
@@ -71,14 +72,16 @@ export class EditPersonaComponent implements OnInit, AfterViewInit {
   getPersona(): void {
     this.persService.getPersona(this.idPersonaLogged).subscribe(
       (data) => {
-        //this.expLab.persona = data;
         this.Persona = data;
-        //console.log(this.project);
       },
       (err) => {
-        alert('No se pudo encontrar a la persona');
+        //alert('No se pudo encontrar a la persona');
+        Swal.fire(
+          'No se pudo encontrar a la persona',
+          'Volver al perfil',
+          'error'
+        );
         this.router.navigate(['/perfil/' + this.idPersonaLogged]);
-        //this.router.navigate(['']);
       }
     );
   }
@@ -93,13 +96,19 @@ export class EditPersonaComponent implements OnInit, AfterViewInit {
         .EditPersona(this.idPersonaLogged, this.Persona)
         .subscribe(
           (data) => {
-            console.log('Persona actualizada: ', data);
-            alert('Datos Actualizados');
+            //console.log('Persona actualizada: ', data);
+            //alert('Datos Actualizados');
+            Swal.fire('Datos Actualizados', 'Press Ok', 'success');
             this.router.navigate(['perfil/' + this.idPersonaLogged]);
           },
           (err) => {
             this.IsLoadding = false;
-            alert('Fallo la operacion');
+            //alert('Fallo la operacion');
+            Swal.fire(
+              'Error al modificar el registro',
+              'Vuelva a intentarlo',
+              'error'
+            );
             console.log(err);
           }
         );
